a5493111ee2f23a59f3ac26c0f1268daed961c3f
imtauseef <tauseefkhan532@gmail.com>
2021-08-19 12:04:43 +0500

    added frontend and requirements file

--- home_app/urls.py
+++ home_app/urls.py
@@ -3,4 +3,5 @@ from . import views
 
 urlpatterns = [
     path('', views.home, name="home"),
+    path('new_search', views.search, name="new_search"),
 ]
--- home_app/views.py
+++ home_app/views.py
@@ -2,4 +2,13 @@ from django.shortcuts import render
 
 # Create your views here.
 def home(request):
-    return render(request, template_name="home_app/base.html")
+    return render(request, template_name="base.html")
+
+
+def search(request):
+    searched_things = request.POST.get('search')
+    context = {
+        'searched_things': searched_things,
+    }
+
+    return render(request, "home_app/search.html", context)
